31b496331741b0d2aae094887402b1d644f833db
Jun Marcel Fritz <jun.fritz@gmail.com>
2019-07-08 17:44:06 -0700

    add static files for website + deployFrontend logic for custom resource trigger.

--- src/deployFrontend/index.js
+++ src/deployFrontend/index.js
@@ -1,6 +1,45 @@
-exports.handler = async (event, context) => {
-  // Log the event argument for debugging and for use in local development.
-  console.log(JSON.stringify(event, undefined, 2));
+const fs = require('fs');
+const path = require('path');
+const AWS = require('aws-sdk');
+const cfnCR = require('cfn-custom-resource');
+const mime = require('mime-types');
+const recursiveReaddir = require('recursive-readdir');
 
-  return {};
+const s3 = new AWS.S3();
+
+exports.handler = async message => {
+  try {
+    await uploadStaticContent();
+
+    // Send success signal back to CloudFormation
+    await cfnCR.sendSuccess('deployFrontend', {}, message);
+
+    console.log('Succeeded in uploading site content!')
+  } catch (err) {
+    console.error('Failed to upload site content:');
+    console.error(err);
+
+    // Send error message back to CloudFormation
+    await cfnCR.sendFailure(err.message, message);
+
+    // Re-throw error to ensure invocation is marked as a failure
+    throw err;
+  }
 };
+
+// Upload site content from 'static' directory
+async function uploadStaticContent() {
+  // List files in 'static' directory
+  const files = await recursiveReaddir('static');
+
+  // Upload files asynchronously to frontend content object store
+  const promises = files.map(file => s3.putObject({
+    Bucket: process.env.BUCKET_NAME,
+    Key: path.relative('static', file),
+    Body: fs.createReadStream(file),
+    ContentType: mime.lookup(file) || 'application/octet-stream',
+    ACL: 'public-read'
+  }).promise());
+
+  await Promise.all(promises);
+}
---
+++ src/deployFrontend/static/main.js
@@ -0,0 +1,59 @@
+// Add your API endpoint
+const API_ENDPOINT = "https://q638nucor2.execute-api.us-east-2.amazonaws.com/local";
+const time = new Date();
+
+//AJAX POST
+$('#submit').click(function(e) {
+  e.preventDefault();
+
+  const inputData = {
+    "content": $('#itemInput').val(),
+    "timestamp": time.toLocaleString()
+    };
+
+  var ajaxRequest = $.ajax({
+      type: "POST",
+      url: `${API_ENDPOINT}/new-item`,
+      data: JSON.stringify(inputData),
+      contentType: "application/json",
+      dataType: "text",
+      success: function (response) {
+        console.log(response);
+      }
+    });
+
+  //When the request successfully finished, execute passed in function
+  ajaxRequest.done(function(response){
+    document.getElementById("itemAdded").innerHTML = "Item Added!";
+  });
+
+  //When the request failed, execute the passed in function
+  ajaxRequest.fail(function(jqXHR, status){
+    alert(`Something went wrong: ${JSON.stringify(status)}`);
+  });
+});
+
+//AJAX GET REQUEST
+$('#getItems').click(function (e) {
+  e.preventDefault();
+  $.ajax({
+    url: `${API_ENDPOINT}/items`,
+    crossDomain: true,
+    dataType: 'json',
+    type: 'GET',
+    contentType: 'application/json',
+    success: function (response) {
+      $('#itemsTable tr').slice(1).remove();
+      jQuery.each(response, function(i,data) {
+        $("#itemsTable").append("<tr> \
+            <th scope='row'>" + data['id'] + "</td> \
+            <td>" + data['content'] + "</td> \
+            <td>" + data['timestamp'] + "</td> \
+            </tr>");
+      });
+    },
+    error: function () {
+      alert("Something went wrong");
+    }
+  });
+});
