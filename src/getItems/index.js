a75043f0b9ffe4d6085f806360beea7dfdf6d764
Jun Marcel Fritz <jun.fritz@gmail.com>
2019-07-08 17:45:28 -0700

    add getItems function with dynamodb scan call.

--- src/getItems/index.js
+++ src/getItems/index.js
@@ -1,6 +1,32 @@
-exports.handler = async (event, context) => {
-  // Log the event argument for debugging and for use in local development.
-  console.log(JSON.stringify(event, undefined, 2));
+const AWS = require('aws-sdk');
 
-  return {};
+exports.handler = async () => {
+  // Use dynamodb to get items from the ItemTable
+  const dynamodb = new AWS.DynamoDB.DocumentClient();
+  const params = {
+    TableName: process.env.TABLE_NAME
+  };
+
+  let allItems = [];
+
+  try {
+    console.log(`Getting data from table ${process.env.TABLE_NAME}.`);
+    items = await dynamodb.scan(params).promise(); // get items from DynamoDB
+    items.Items.forEach((item) => allItems.push(item)); // put contents in an array for easier parsing
+    allItems.forEach(item => console.log(`Item ${item.id}: ${item.content}\n`)); // log the contents
+  } catch (error) {
+    console.log(`Error getting data from table ${process.env.TABLE_NAME}. Make sure this function is running in the same environment as the table.`);
+    throw new Error(error); // stop execution if data from dynamodb not available
+  }
+
+  const response = {
+    statusCode: "200",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
+    },
+    body: JSON.stringify(allItems),
+    isBase64Encoded: false
+  }
+  return response;
 };
