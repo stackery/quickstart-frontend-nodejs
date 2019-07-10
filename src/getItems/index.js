0b30b759f78b39aa5886cc196354f2613b216456
Anna Spysz <anna.spysz@stackery.io>
2019-07-10 12:19:06 -0700

    Adding back logging

--- src/getItems/index.js
+++ src/getItems/index.js
@@ -13,6 +13,7 @@ exports.handler = async () => {
     console.log(`Getting data from table ${process.env.TABLE_NAME}.`);
     items = await dynamodb.scan(params).promise(); // get items from DynamoDB
     items.Items.forEach((item) => allItems.push(item)); // put contents in an array for easier parsing
+    allItems.forEach(item => console.log(`Item ${item.id}: ${item.content}\n`)); // log the contents
   } catch (error) {
     console.log(`Error getting data from table ${process.env.TABLE_NAME}. Make sure this function is running in the same environment as the table.`);
     throw new Error(error); // stop execution if data from dynamodb not available
